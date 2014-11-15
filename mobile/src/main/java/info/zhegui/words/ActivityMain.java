723fc23056a27459c1f056093211ea4cf41907b6
xinanadu <xinanadu@foxmail.com>
2014-11-15 15:22:51 +0800

    记住的单词下次不再显示

--- mobile/src/main/java/info/zhegui/words/ActivityMain.java
+++ mobile/src/main/java/info/zhegui/words/ActivityMain.java
@@ -1,15 +1,22 @@
 package info.zhegui.words;
 
+import android.content.Intent;
 import android.os.Handler;
 import android.os.Message;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Adapter;
 import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
@@ -23,22 +30,26 @@ import java.util.List;
 
 public class ActivityMain extends ActionBarActivity {
 
-    private ArrayList<String> listWordsTemp = new ArrayList<String>();
-    private ArrayList<String> listWords = new ArrayList<String>();
+    private ArrayList<Word> listWord = new ArrayList<Word>();
+    private ArrayList<String> listString = new ArrayList<String>();
 
+    private final int REQUEST_WORD = 201;
     private final int WHAT_SHOW_WORDS = 101;
 
     private ListView mListView;
     private ArrayAdapter<String> mAdapter;
+
+    private int currentPosition = 0;
 
     private Handler mHandler = new Handler() {
         @Override
         public void handleMessage(Message msg) {
             switch (msg.what) {
                 case WHAT_SHOW_WORDS:
-                    listWords.clear();
-                    for (String word : listWordsTemp) {
-                        listWords.add(word);
+                    listString.clear();
+                    for (Word word : listWord) {
+                        if (!word.remember)
+                            listString.add(word.key);
                     }
                     mAdapter.notifyDataSetChanged();
                     break;
@@ -55,8 +66,54 @@ public class ActivityMain extends ActionBarActivity {
         loadWords();
 
         mListView = (ListView) findViewById(R.id.listview);
-        mAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_expandable_list_item_1, listWords);
+        mAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_expandable_list_item_1, listString);
         mListView.setAdapter(mAdapter);
+
+        findViewById(R.id.btn_start).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivityWord();
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_WORD) {
+            if (resultCode == RESULT_OK) {
+                log("-->remember");
+                Word word = listWord.get(currentPosition);
+                word.remember = true;
+                listWord.set(currentPosition, word);
+                mHandler.sendEmptyMessage(WHAT_SHOW_WORDS);
+            } else {
+                log("-->forget");
+
+            }
+
+            currentPosition++;
+            while (currentPosition < listWord.size() && listWord.get(currentPosition).remember) {
+                currentPosition++;
+            }
+            if (currentPosition < listWord.size()) {
+                startActivityWord();
+            } else {
+//                toast("no more");
+
+            }
+
+        }
+    }
+
+    private void startActivityWord() {
+        if (currentPosition < listWord.size()) {
+            String currentWord = listWord.get(currentPosition).key;
+            log("---curent word:" + currentWord);
+            Intent intent = new Intent(ActivityMain.this, ActivityWord.class);
+            intent.putExtra("word", currentWord);
+            startActivityForResult(intent, REQUEST_WORD);
+            overridePendingTransition(R.anim.fade_in_slow, R.anim.fade_out_slow);
+        }
     }
 
     private void loadWords() {
@@ -66,11 +123,11 @@ public class ActivityMain extends ActionBarActivity {
                     InputStream is = ActivityMain.this.getResources().openRawResource(R.raw.words);
                     BufferedReader br = new BufferedReader(new InputStreamReader(is));
                     String str = null;
-                    listWordsTemp.clear();
+                    listWord.clear();
                     ;
                     while ((str = br.readLine()) != null) {
                         log(str);
-                        listWordsTemp.add(str);
+                        listWord.add(new Word(str, false));
                     }
                     br.close();
                     is.close();
@@ -104,7 +161,56 @@ public class ActivityMain extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    public class Word {
+        public String key;
+        public boolean remember;
+
+        public Word(String key, boolean remember) {
+            this.key = key;
+            this.remember = remember;
+        }
+    }
+
+    static class ViewHolder {
+        TextView tvKey;
+    }
+
+    public class WordAdapter extends BaseAdapter {
+        @Override
+        public int getCount() {
+            return listString.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return listString.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            ViewHolder holder = null;
+            if (convertView == null) {
+                holder = new ViewHolder();
+                holder.tvKey = (TextView) LayoutInflater.from(ActivityMain.this).inflate(android.R.layout.simple_expandable_list_item_1, parent, false);
+                convertView.setTag(holder);
+            } else {
+                holder = (ViewHolder) convertView.getTag();
+            }
+            holder.tvKey.setText(listString.get(position));
+            return holder.tvKey;
+        }
+    }
+
     private void log(String msg) {
         Log.d("ActivityMain", msg);
+    }
+
+    private void toast(String text) {
+        Toast.makeText(this, text, Toast.LENGTH_LONG).show();
     }
 }
--- mobile/src/main/java/info/zhegui/words/ActivityWord.java
+++ mobile/src/main/java/info/zhegui/words/ActivityWord.java
@@ -1,19 +1,150 @@
 package info.zhegui.words;
 
+import android.app.Activity;
+import android.os.CountDownTimer;
+import android.os.Handler;
+import android.os.Message;
+import android.support.v4.view.GestureDetectorCompat;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.GestureDetector;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.Random;
 
 
 public class ActivityWord extends ActionBarActivity {
+
+    private GestureDetectorCompat mDetector;
+
+    private CountDownTimer mCountDownTimer;
+
+    private final int WHAT_SHOW_COUNTDOWN = 101;
+
+    private TextView tvCountDown;
+
+    private Handler mHandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case WHAT_SHOW_COUNTDOWN:
+                    int countDown = msg.arg1;
+                    tvCountDown.setText("Fade out in " + countDown + " seconds");
+                    break;
+            }
+        }
+    };
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        this.mDetector.onTouchEvent(event);
+        return super.onTouchEvent(event);
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_word);
+
+        mDetector = new GestureDetectorCompat(this, new MyGestureListener());
+
+        tvCountDown = (TextView) findViewById(R.id.tv_count_down);
+
+        String word = getIntent().getStringExtra("word");
+        TextView tvWord = (TextView) findViewById(R.id.tv_word);
+        tvWord.setText(word);
+
+        LinearLayout layoutWordContainer = (LinearLayout) findViewById(R.id.layout_word_container);
+        int gravity = Gravity.TOP;
+        Random random=new Random();
+        int randomInt=random.nextInt(9);
+        if (randomInt<3) {
+            gravity=Gravity.BOTTOM;
+        } else if(randomInt>5){
+            gravity=Gravity.TOP;
+        } else {
+            gravity=Gravity.CENTER;
+        }
+        layoutWordContainer.setGravity(gravity);
+
+        findViewById(R.id.btn_yes).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish(RESULT_OK);
+            }
+        });
+        findViewById(R.id.btn_no).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish(RESULT_CANCELED);
+            }
+        });
+
+        mCountDownTimer = new CountDownTimer(4000, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                int countDown = (int) (millisUntilFinished + 500) / 1000;
+                Message msg = mHandler.obtainMessage(WHAT_SHOW_COUNTDOWN, countDown, 0);
+                msg.sendToTarget();
+            }
+
+            @Override
+            public void onFinish() {
+                int countDown = 0;
+                Message msg = mHandler.obtainMessage(WHAT_SHOW_COUNTDOWN, countDown, 0);
+                msg.sendToTarget();
+                finish(RESULT_CANCELED);
+            }
+        };
+        mCountDownTimer.start();
     }
 
+    private void finish(int result) {
+        if (mCountDownTimer != null) {
+            mCountDownTimer.cancel();
+            setResult(result);
+            finish();
+        }
+    }
+
+    @Override
+    public void finish() {
+        super.finish();
+
+        overridePendingTransition(R.anim.fade_in_slow, R.anim.fade_out_slow);
+    }
+
+    class MyGestureListener extends GestureDetector.SimpleOnGestureListener {
+        private static final String DEBUG_TAG = "Gestures";
+
+
+        @Override
+        public boolean onFling(MotionEvent event1, MotionEvent event2,
+                               float velocityX, float velocityY) {
+            log("onFling: " + event1.toString() + event2.toString());
+
+            log("event2.getY()-event1.getY():" + (event2.getY() - event1.getY()));
+            final int EX = 20;
+            if (event2.getY() - event1.getY() < -1 * EX) {
+                //向上滑动，表示否
+                log("up");
+                finish(RESULT_CANCELED);
+            } else if (event2.getY() - event1.getY() > 1 * EX) {
+                //向下，表示是
+                log("down");
+                finish(RESULT_OK);
+            }
+            return true;
+        }
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -32,5 +163,9 @@ public class ActivityWord extends ActionBarActivity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    private void log(String msg) {
+        Log.d("ActivityWord", msg);
     }
 }
